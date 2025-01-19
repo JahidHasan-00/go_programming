1015f086ef5d695e6937854d5e214bba1719efb3
JahidHasan-00 <jahid.hasan50001@gmail.com>
2025-01-19 07:21:34 +0600

    Defined GO-programming structure with example

---
+++ main.go
@@ -0,0 +1,26 @@
+package main // Package main: must package main.go file --> main
+
+import "fmt" // fmt => format
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+/** Go Syntax
+	A Go file consists of the following parts:
+		->Package declaration
+		->Import packages
+		->Functions
+		->Statements and expressions
+
+	Example explained:
+
+	Line 1: In Go, every program is part of a package. We define this using the package keyword. In this example, the program belongs to the main package.
+	Line 2: import ("fmt") lets us import files included in the fmt package.
+	Line 3: A blank line. Go ignores white space. Having white spaces in code makes it more readable.
+	Line 4: func main() {} is a function. Any code inside its curly brackets {} will be executed.
+	Line 5: fmt.Println() is a function made available from the fmt package. It is used to output/print text. In our example it will output "Hello World!".
+
+	Note: In Go, any executable code belongs to the main package.
+
+**/
