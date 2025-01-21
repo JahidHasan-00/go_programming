9dcde17742562bf4ca7c2ddd90a9bebbc4fdfa5e
JahidHasan-00 <jahid.hasan50001@gmail.com>
2025-01-22 00:35:38 +0600

    Declared Single return and multiple return value function

--- function/main.go
+++ function/main.go
@@ -7,6 +7,33 @@ func subtraction(a int, b int) {
 	fmt.Println("Subtraction: ", sub)
 }
 
+/**Function with Return Values and Types:
+   --> Function with return values and types are used to return a value from a function.
+**/
+//Single return value function
+func multiplication(a int, b int) int { // a = 10, b = 20
+	mul := a * b
+	return mul
+}
+
+//Multiple return value function
+func getNumbers(a int, b int) (int, int) {
+	mod := a % b
+	div := a / b
+	return mod, div
+}
+
+//String return value function
+func getMyName(name string) {
+	fmt.Println("My Name is : ", name)
+}
+
+//
+func getMyFullName(firstName, lastName string) string {
+	fullName := firstName + " " + lastName
+	return fullName
+}
+
 func main() {
 	a := 10
 	b := 20
@@ -14,4 +41,15 @@ func main() {
 	fmt.Println("Summation: ", sum)
 	//Now calculate the subtraction operation from another function
 	subtraction(a, b)
+	subtraction(30, 50)
+	//Calculate the multiplication operation
+	mul := multiplication(a, b)
+	fmt.Println("multiplication: ", mul)
+	//Another Example:
+	mod, div := getNumbers(a, b)
+	fmt.Println("Modulas: ", mod)
+	fmt.Println("Division: ", div)
+	getMyName("Jahid")
+	fullName := getMyFullName("Jahid", "Hasan")
+	fmt.Println("Full Name: ", fullName)
 }
