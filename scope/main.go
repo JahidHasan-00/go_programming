2618ed0bd94414e6a0951ecc88ffcf3c6251cfae
JahidHasan-00 <jahid.hasan50001@gmail.com>
2025-01-25 18:50:47 +0600

    Practiced Scope Global, Local and Block Scope

--- scope/main.go
+++ scope/main.go
@@ -9,11 +9,36 @@ import (
 var (
 	num1 = 10
 	num2 = 20
+	num3 = 30
 )
 
+// const num3 = 20
+
+//   num3 = 30 // This will throw an error because we cannot reassign a value to a constant
+
 func main() {
+	num2 = 10
+	var num3 = 40 // Local Scope variable num3
 	fmt.Println("This is the main function body and this is the local scope")
 	add(num1, num2)
 	fmt.Println("Showing Custom Package")
 	mathlib.Multiplication(num1, num2)
+	getValue(num3)
+
+	// Block Scope
+	if num1 == num2 {
+		fmt.Println("num1 is equal to num2")
+	} else if num2 == num3 {
+		fmt.Println("num2 is equal to num3")
+
+	} else {
+		x := 10
+		fmt.Println("num1 is not equal to num2")
+		fmt.Println("But x is equal to num1: ", x, "=", num1)
+	}
+	// fmt.Println(x); // Block scope variable x is not accessible here
+}
+
+func getValue(x int) {
+	fmt.Println(x)
 }
