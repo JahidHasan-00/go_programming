ed81982fe405c5d070a55ff5eff98b3fed40d960
JahidHasan-00 <jahid.hasan50001@gmail.com>
2025-02-01 13:10:56 +0600

    Created a function that returns an anonymous function

--- functions/anonymous-and-iife-func/main.go
+++ functions/anonymous-and-iife-func/main.go
@@ -12,14 +12,32 @@ func add(x, y int) int {
 	return x + y - x*y
 }
 
+//Function returns an anonymous function
+func multiplier(factor int) func(int) int {
+
+	return func(a int) int {
+		return a * factor
+	}
+}
+
 func main() {
 	result := add(10, 20)
+
+	result1 := multiplier(5)
+	fmt.Println(result1(5))
 
 	//Anonymous Function
 	//IIFE (Immediately Invoked Function Expression)
 	func(res int) {
 		fmt.Println("Sum of those values: ", res)
 	}(result)
+
+	//Assigning an anonymous function to a variable
+	greet := func(name string) {
+		fmt.Println("Hello, ", name)
+	}
+	greet("John")
+	greet("Doe")
 }
 
 func init() {
