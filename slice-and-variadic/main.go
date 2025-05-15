d4e17a59d128c41c26beabf93cdfc2ce5280857e
JahidHasan-00 <jahid.hasan50001@gmail.com>
2025-05-16 02:14:18 +0600

    Defined variadic function and it takes multiple arguments

--- slice-and-variadic/main.go
+++ slice-and-variadic/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func changeSlice(a []int) []int {
+	a[0] = 10
+	a = append(a, 11)
+	return a
+}
+
+func print(a ...int) {
+	fmt.Println(a)
+}
+
 func main() {
 	// arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	// fmt.Println(arr)
@@ -22,12 +32,63 @@ func main() {
 	// sl := []int{1, 2, 5}                                        //pointer, length, capacity
 	// fmt.Println("slice:", sl, "len:", len(sl), "cap:", cap(sl)) // [1 2 5] len: 3 cap: 3
 
-	sm := make([]int, 3) // [0 2 0] len:3 cap:3
-	sm[1] = 2
+	// sm := make([]int, 3) // [0 2 0] len:3 cap:3
+	// sm[1] = 2
 
-	fmt.Println(sm)      // [0 2 0]
-	fmt.Println(len(sm)) // 3
-	fmt.Println(cap(sm)) // 3.
+	// fmt.Println(sm)      // [0 2 0]
+	// fmt.Println(len(sm)) // 3
+	// fmt.Println(cap(sm)) // 3.
+
+	//Another way to use make function:
+	// sm1 := make([]int, 4, 5) // [0 0 0 0] len: 4, cap: 5
+
+	// sm1[1] = 5
+	// sm1[0] = 2
+	// sm1[3] = 6
+
+	// fmt.Println(sm1)      // [2 5 0 6], pointer: indexOf-0 of memory address
+	// fmt.Println(len(sm1)) // len: 4
+	// fmt.Println(cap(sm1)) // cap: 5
+
+	// sm2 := sm1[1:4] //[5 0 6]
+
+	// fmt.Println(sm2)      // [5 0 6]
+	// fmt.Println(len(sm2)) // 3
+	// fmt.Println(cap(sm2)) // 4
+
+	// Empty slice or nil slice:
+	// var x []int      // pointer = nil, length = 0, cap = 0
+	// x = append(x, 1) // [1], len = 1, cap = 1
+	// x = append(x, 2) // [1, 2], len = 2, cap = 2
+	// x = append(x, 3) // [1, 2, 3], len = 3, cap = 4
+
+	// y := x // [1, 2, 3], len = 3, cap = 4
+
+	// x = append(x, 4)
+	// y = append(y, 5)
+
+	// x[0] = 10
+
+	// fmt.Println(x, len(x), cap(x)) //[1 2 3 5], len = 4, cap = 4
+	// fmt.Println(y, len(y), cap(y)) //[1 2 3 5], len = 4, cap = 4
+	// // fmt.Println(len(x))    //
+	// // fmt.Println(cap(x))    // 3
+
+	//Another Example:
+	x1 := []int{1, 2, 3, 4, 5}
+	x1 = append(x1, 6)
+	x1 = append(x1, 7)
+
+	a := x1[4:]
+	y1 := changeSlice(a)
+
+	fmt.Println(x1)      // [1 2 3 4 10 6 7]
+	fmt.Println(y1)      // [10 6 7 11]
+	fmt.Println(x1[0:8]) // [1 2 3 4 10 6 7 11]
+
+	//Pass argument for variadic function:
+	print(5, 10, 12)
+
 }
 
 /**
@@ -42,4 +103,6 @@ func main() {
 	2. slice from slice
 	3. slice literal
 	4. slice make() with length
+	5. slice make() with length and capacity
+	6. Empty slice or nil slice using append() function
 ***/
